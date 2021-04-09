c7353a2619f14d35915df377a37b3a8f085b6cad
Kevin Moore <kevmoo@google.com>
2021-04-09 12:08:48 -0700

    cleanup

--- whatsup.dart
+++ whatsup.dart
@@ -1,30 +1,28 @@
 #!/usr/bin/env dart
 
 import 'package:args/args.dart';
+import 'package:completion/completion.dart';
 import 'package:logging/logging.dart';
-import 'package:bot_io/completion.dart';
-import 'package:bot_io/bot_io.dart';
 
 final _log = new Logger('whats_up');
 
 void main(List<String> args) {
-  enableScriptLogListener();
   _log.info('app starting');
 
   final parser = _getParser();
 
   final results = tryArgsCompletion(args, parser);
 
-  if(results.command != null && results.command.name == 'help') {
+  if (results.command != null && results.command.name == 'help') {
     _log.info('help requested');
     print("how to use what's up dart");
-    print(parser.getUsage());
+    print(parser.usage);
     return;
   }
 
   final names = results.rest.toList();
 
-  if(names.isEmpty) {
+  if (names.isEmpty) {
     _log.info('no name provided');
     names.add('World');
   }
@@ -33,10 +31,10 @@ void main(List<String> args) {
 
   final String greeting = results['greeting'];
 
-  for(final name in names) {
+  for (final name in names) {
     var msg = "$greeting, $name?";
 
-    if(isYelling) {
+    if (isYelling) {
       msg = msg.toUpperCase();
     }
 
@@ -46,9 +44,12 @@ void main(List<String> args) {
   _log.info('app finished');
 }
 
-ArgParser _getParser() =>
-  new ArgParser()
-    ..addFlag('yell', abbr: 'y', help: 'Do you want me to yell?', defaultsTo: false, negatable: true)
-    ..addOption('greeting', abbr: 'g', help: 'Define a custom greeting', defaultsTo: "What's up")
-    ..addCommand('help');
-
+ArgParser _getParser() => new ArgParser()
+  ..addFlag('yell',
+      abbr: 'y',
+      help: 'Do you want me to yell?',
+      defaultsTo: false,
+      negatable: true)
+  ..addOption('greeting',
+      abbr: 'g', help: 'Define a custom greeting', defaultsTo: "What's up")
+  ..addCommand('help');
